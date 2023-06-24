eb9ff598e137b6c531c6e07ea5f0c6226caebb9d
Tina Pham <tinapham@Tinas-MBP.attlocal.net>
2023-06-24 00:53:18 -0500

    added a template and home page

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Tina'}
-    return '''
-<html>
-    <head>
-        <title>Home Page - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-    </body>
-<html>'''
+    posts = [
+        {
+            'author': {'username': 'Edmond'},
+            'body': 'Beautiful day in Hawaii!'
+        },
+        {
+            'author': {'username': 'Beau'},
+            'body': 'I like to go on walks!'
+        }
+    ]
+    return render_template('index.html', title="Home", user=user, posts=posts)
