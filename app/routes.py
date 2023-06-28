eab906c5f3344ca3874bea0cf46aad61b1d36964
My Name <tinapham@Tinas-MBP.attlocal.net>
2023-06-27 23:25:01 -0500

    test

--- app/routes.py
+++ app/routes.py
@@ -27,3 +27,4 @@ def login():
             form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+def test():
