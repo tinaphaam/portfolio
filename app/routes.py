0a2e0421c11bdf8bbb9bfd37d8204143d3ac27d0
tinaa.pham@gmail.com <tinapham@Tinas-MBP.attlocal.net>
2023-06-27 23:26:35 -0500

    test

--- app/routes.py
+++ app/routes.py
@@ -27,4 +27,3 @@ def login():
             form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
-def test():
